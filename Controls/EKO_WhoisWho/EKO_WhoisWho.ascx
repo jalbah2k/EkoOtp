d555d43314aa36dac0aa2689b3402cf1ff38bbd5
Juan <Juan@CL-T097-194CN>
2026-08-20 12:44:02 -0400

    EKO_WhoisWho

---
+++ Controls/EKO_WhoisWho/EKO_WhoisWho.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="EKO_WhoisWho.ascx.cs" Inherits="EKO_WhoisWho" %>
+EKO_WhoisWho
---
+++ Controls/EKO_WhoisWho/EKO_WhoisWho.ascx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class EKO_WhoisWho : System.Web.UI.UserControl
+{
+    public EKO_WhoisWho() { }
+    public EKO_WhoisWho(string p) { }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
